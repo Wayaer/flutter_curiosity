8c48d2a318b323138a1d84b6a41c247179acd58a
Wayaer <wayaer@foxmail.com>
2019-12-14 00:20:39 +0800

    updata code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,30 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_curiosity/curiosity.dart';
 
-void main() => runApp(App());
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: App(),
+  ));
+}
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text('Flutter Curiosity Plugin example app'),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Center(),
-            RaisedButton(
-                onPressed: () {
-                  getVersion(context);
-                },
-                child: Text('按钮'))
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Flutter Curiosity Plugin example app'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Center(),
+          RaisedButton(
+              onPressed: () {
+                getVersion(context);
+              },
+              child: Text('按钮'))
+        ],
       ),
     );
   }
