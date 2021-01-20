0f2f10954dae7268258f3bcbaaab7b781694c855
Wayaer <wayaer@foxmail.com>
2021-01-20 18:26:22 +0800

    add windows

---
+++ example/android/app/src/main/kotlin/com/curiosity/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.curiosity.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/flutter_curiosity_web.dart
+++ lib/flutter_curiosity_web.dart
@@ -1,1 +1,14 @@
-class CuriosityPlugin {}
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+
+class CuriosityPlugin extends PlatformInterface {
+
+
+  static void registerWith(Registrar registrar) {
+
+  }
+
+}
+
+abstract class CuriosityPlatform extends PlatformInterface {
+
+}
