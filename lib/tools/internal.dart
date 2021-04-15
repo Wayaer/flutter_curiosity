2fd3eb97af635afc8452143370b35cdc83c5fe45
Wayaer <wayaer@foxmail.com>
2021-04-15 12:17:59 +0800

    format

--- lib/tools/internal.dart
+++ lib/tools/internal.dart
@@ -3,18 +3,14 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_curiosity/platform/platform.dart';
 
+final MediaQueryData mediaQueryData = MediaQueryData.fromWindow(window);
+
 /// size → Size 设备尺寸信息，如屏幕的大小，单位 pixels
-Size get getWindowSize =>
-    MediaQueryData
-        .fromWindow(window)
-        .size;
+Size get getWindowSize => mediaQueryData.size;
 
 /// devicePixelRatio → double 单位逻辑像素的设备像素数量，即设备像素比。
 /// 这个数字可能不是2的幂，实际上它甚至也可能不是整数。例如，Nexus 6的设备像素比为3.5。
-double get getDevicePixelRatio =>
-    MediaQueryData
-        .fromWindow(window)
-        .devicePixelRatio;
+double get getDevicePixelRatio => mediaQueryData.devicePixelRatio;
 const int _limitLength = 800;
 
 void log<T>(T msg) {
